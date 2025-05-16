d550abf88bf898d3315e326fbae50dc1c7720d16
sudritghimire-ai <sudritghimire@gmail.com>
2025-05-16 10:38:50 +0545

    Update blogsApi.js

--- src/redux/features/blogs/blogsApi.js
+++ src/redux/features/blogs/blogsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const blogApi = createApi({
   reducerPath: "blogsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://springfallusa-76wx.onrender.com/api/',
+    baseUrl: 'https://springfallusa.onrender.com/api/',
     credentials: 'include',
    prepareHeaders: (headers) => {
   const token = localStorage.getItem('token');
