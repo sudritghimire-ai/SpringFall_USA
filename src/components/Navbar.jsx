4e7095911b2988f1a7c2d1cbd77c8228d7cac612
sudritghimire-ai <sudritghimire@gmail.com>
2025-05-24 07:25:50 +0545

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,13 +58,13 @@ const Navbar = () => {
                 </a>
 
                 {/* Desktop Nav Items */}
-                <ul className='hidden sm:flex items-center gap-x-10'>
+                <ul className='hidden sm:flex items-center gap-x-8'>
                     {navLists.map((list, index) => (
                         <li key={index} className='flex-shrink-0'>
                             <NavLink
                                 to={list.path}
                                 className={({ isActive }) =>
-                                    `relative px-4 py-2 overflow-hidden group rounded-md font-medium text-lg ${
+                                    `relative px-3 py-2 overflow-hidden group rounded-md font-medium text-base ${
                                         isActive ? 'text-[#1E73BE] font-semibold' : 'text-gray-700'
                                     }`
                                 }
