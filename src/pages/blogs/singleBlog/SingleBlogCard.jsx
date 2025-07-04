506367c67ccd251df9eab9388d67acc85019900d
sudritghimire-ai <sudritghimire@gmail.com>
2025-07-04 18:05:55 +0545

    Update SingleBlogCard.jsx

--- src/pages/blogs/singleBlog/SingleBlogCard.jsx
+++ src/pages/blogs/singleBlog/SingleBlogCard.jsx
@@ -426,7 +426,7 @@ const SingleBlogCard = ({ blog }) => {
 
         {/* Main Content */}
 
-<div className="flex-1 lg:ml-[24rem]"> 
+<div className="flex-1 lg:ml-[21rem]">
 
 <article className="max-w-4xl mx-auto px-4 sm:px-6 py-8 lg:px-12 font-outfit border-0 shadow-none lg:pl-4">
             {/* Header Section */}
