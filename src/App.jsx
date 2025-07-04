70f04122fe19787c87103ebef1f069df2fd88e86
sudritghimire-ai <sudritghimire@gmail.com>
2025-07-04 16:28:07 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
 import './App.css';
 
 function App() {
+  const location = useLocation();
+
+  // hide navbar for single blog detail pages
+  const isBlogDetailPage = /^\/blogs\/[a-zA-Z0-9]+$/.test(location.pathname);
+
   return (
     <>
       <div className="bg-bgPrimary min-h-screen flex flex-col">
-        <Navbar />
+        {!isBlogDetailPage && <Navbar />}
         <div className="flex-grow">
           <Outlet />
         </div>
