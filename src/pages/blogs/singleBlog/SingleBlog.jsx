fa60540e81b25b0a4121535ad4618d535dcc5a83
sudritghimire-ai <sudritghimire@gmail.com>
2025-07-11 10:08:14 +0545

    Update SingleBlog.jsx

--- src/pages/blogs/singleBlog/SingleBlog.jsx
+++ src/pages/blogs/singleBlog/SingleBlog.jsx
@@ -33,7 +33,7 @@ const SingleBlog = () => {
 
 
               {/* Adjust related blogs width */}
-             <div className="lg:w-1/4 w-full mt-6 lg:mt-0 lg:ml-10">
+             <div className="lg:w-1/4 w-full mt-6 lg:mt-0 lg:ml-12">
   <RelatedBlogs />
 </div>
 
