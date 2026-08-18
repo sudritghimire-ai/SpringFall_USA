360f239ef5a679f89f6f7a233a52a362f6d10fb1
sudritghimire-ai <sudritghimire@gmail.com>
2026-08-18 00:46:07 -0500

    Update SingleBlog.jsx

--- src/pages/blogs/singleBlog/SingleBlog.jsx
+++ src/pages/blogs/singleBlog/SingleBlog.jsx
@@ -10,7 +10,7 @@ const SingleBlog = () => {
   const { data: blog, error, isLoading } = useFetchBlogByIdQuery(id);
 
   return (
-    <div className="text-primary container mx-auto mt-24 md:mt-20 px-4">
+    <div className="text-primary container mx-auto mt-24 md:mt-20 px-4 lg:pl-80">
       {isLoading && (
         <div className="flex justify-center items-center py-20">
           <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-600"></div>
@@ -21,14 +21,14 @@ const SingleBlog = () => {
       {error && <div className="text-red-600">Something went wrong...</div>}
 
       {blog?.post && (
-        <div className="flex flex-col lg:flex-row justify-center items-start gap-8 max-w-[1600px] mx-auto">
-          {/* Main Blog Content Container */}
-          <div className="lg:w-2/3 w-full">
+        <div className="flex flex-col lg:flex-row items-start gap-8 w-full">
+          {/* Main Blog Content */}
+          <div className="flex-1 min-w-0">
             <SingleBlogCard blog={blog.post} />
           </div>
 
-          {/* Related Institutions / Blogs Sidebar */}
-          <div className="lg:w-1/4 w-full mt-6 lg:mt-0">
+          {/* Right Sidebar (Related Institutions) */}
+          <div className="lg:w-80 w-full shrink-0 mt-6 lg:mt-0">
             <RelatedBlogs />
           </div>
         </div>
